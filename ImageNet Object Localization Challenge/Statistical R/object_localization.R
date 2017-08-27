99d2481532a2c7c4e24a9fa12d5dd49a596376a6
Jessica Mueller <jessie.mueller@gmail.com>
2017-08-26 23:27:50 -0600

    Import tensorflow and dependencies

--- ImageNet Object Localization Challenge/Statistical R/object_localization.R
+++ ImageNet Object Localization Challenge/Statistical R/object_localization.R
@@ -1,5 +1,21 @@
-#TODO - Everything
-#Testing branch Jessie
-#Now testing merging in my branch if Melissa changes master first.
-#Again but now Melissa merges before me.
-#test again
+#Install & load any needed libraries.
+
+#To get access to tensorflow, install via devtools.
+#req Rtools 3.4, reticulate
+#tensorflow reqs 64-bit Python 3.5 or 3.6
+install.packages("devtools")
+library(devtools)
+devtools::install_github("rstudio/tensorflow")
+
+
+library(tensorflow)
+install_tensorflow()
+
+#Confirm it works.
+sess = tf$Session()
+hello <- tf$constant('Hello, Jessie!')
+sess$run(hello)
+
+
+#TODO - Everything Else
+
