3b9627a07f50034ea007308bebace1762e1caeed
omgitsjessie <jessie.mueller@gmail.com>
2017-08-29 23:13:40 -0600

    Clean up unused libraries, add function for standard image processing

--- ImageNet Object Localization Challenge/Statistical R/image_preprocessing.R
+++ ImageNet Object Localization Challenge/Statistical R/image_preprocessing.R
@@ -1,6 +1,5 @@
 #Image preprocessing steps
-#libraries
-library(magick)
+library(magick) #like image magick :)
 
 #init sample url
 url <- "http://farm4.static.flickr.com/3020/2755290967_ee3eb0444c.jpg"
@@ -16,3 +15,15 @@ sized_image <- image_resize(loaded_image, "250x250!")
 #Convert to grayscale.
 gray_image <- image_convert(sized_image, colorspace = "gray")
 #plot(gray_image)
+
+#TODO: build this into a function that reads in from a list of URLs
+#and then passes back image data
+
+process_image <- function(urlPath, resolution = "250x250!") {
+  loaded_image <- image_read(urlPath)
+  sized_image <- image_resize(loaded_image, resolution)
+  gray_image <- image_convert(sized_image, colorspace = "gray")
+  #write out image to some index
+}
+
+process_image(url)
